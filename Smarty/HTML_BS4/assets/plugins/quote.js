1d387117951a46cda3e14bfe569d616e5911b520
K-Hed <jns1ma@outlook.com>
2021-09-24 14:23:44 -0400

    better javascript

--- Smarty/HTML_BS4/assets/plugins/quote.js
+++ Smarty/HTML_BS4/assets/plugins/quote.js
@@ -1,26 +1,36 @@
 let verbose = 1;
+function addOption(selectedElement, text) {
+
+    //const addToDiv = document.getElementsByClassName('row');
+    const optionResidential = document.createElement('option');
+  //addToDiv.appendChild(optionResidential);
+  const optionResidentialText = document.createTextNode(text);
+  optionResidential.appendChild(optionResidentialText);
+  optionResidential.setAttribute('value', 'option');
+  selectedElement.appendChild(optionResidential);
+}
+  
 function validate(sel) {
-    const selectedOption = sel.options[sel.selectedIndex].text;
-    const body = document.body;
-    //if (verbose) { console.log(selectedOption); }
-    if (selectedOption === "Residential") {
-        if (verbose) {
-            console.log(selectedOption);
-        }
-        const selectedResidential = document.createElement('select');
-
-        const optionResidential1 = document.createElement('option');
-        const optionResidentialText1 = document.createTextNode('Number of Apartments 1');
-        optionResidential1.appendChild(optionResidentialText1);
-        optionResidential1.setAttribute('value1', 'option value1');
-        selectedResidential.appendChild(optionResidential1);
-
-        const optionResidential2 = document.createElement('option');
-        const optionResidentialText2 = document.createTextNode('Number of Apartments 2');
-        optionResidential2.appendChild(optionResidentialText2);
-        optionResidential2.setAttribute('value2', 'option value2');
-        selectedResidential.appendChild(optionResidential2);
-
-        body.append(selectedResidential);
+  const selectedOption = sel.options[sel.selectedIndex].text;
+  const body = document.body;
+  if (selectedOption === "Residential") {
+    console.log(selectedOption);
+    //const breakLine = document.createElement('br');
+    const selectedResidential = document.createElement('select');
+ 
+    let array = [ "Number of Apartments", "Number of tenants" ];
+    for (i = 0; i < 2; ++i) {
+      addOption(selectedResidential, array[i]);
     }
+    //body.append(breakLine);
+    //body.append(breakLine);
+
+    //body.append(selectedResidential);
+    //const addToDiv = document.getElementsByClassName('row');
+    //console.log(addToDiv);
+    //addToDiv.appendChild(selectedResidential);
+
+    const addToDiv = document.getElementById('new_option');
+    addToDiv.appendChild(selectedResidential);
+  }
 }
